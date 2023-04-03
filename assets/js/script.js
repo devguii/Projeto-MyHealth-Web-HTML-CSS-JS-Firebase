7408d1dd770ea1017def6df2c4ee3c6ba5979672
gui <guigon@alunos.utfpr.edu.br>
2023-04-03 20:24:36 -0300

    feat: create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+document
+  .getElementById("criar-conta-button")
+  .addEventListener("click", irParaCriarConta);
+
+function irParaCriarConta() {
+  window.location.href = "../src/criar-conta.html";
+}
