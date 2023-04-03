fde1cc9d47a5495e2fe7367bdeb00d00e8acc311
gui <guigon@alunos.utfpr.edu.br>
2023-04-03 20:35:58 -0300

    build: testing DOMContentLoaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+// função que será executada quando a página for carregada
+document.addEventListener("DOMContentLoaded", () => {
+  // adicionando um console.log para testar se a função é executada corretamente
+  console.log("Página carregada com sucesso!");
+});
+
 document
   .getElementById("criar-conta-button")
   .addEventListener("click", irParaCriarConta);
