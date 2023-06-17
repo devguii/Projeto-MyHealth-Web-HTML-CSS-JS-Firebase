6d4e725413cbb07b88d666667aa49efb0a511e90
gui <guigon@alunos.utfpr.edu.br>
2023-06-17 16:24:13 -0300

    add createUserWithEmailAndPassword

--- src/js/criar-conta.js
+++ src/js/criar-conta.js
@@ -1,15 +1,44 @@
 import { auth } from "../firebase/config.js"
+import {createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js"
+
+const emailMessage = document.getElementById("email-message")
+const passwordMessage = document.getElementById("password-message")
 
 const getEmail = () => {
-    return document.getElementById("email-input");
+    return document.getElementById("email-input").value
 }
 
 const getPassword = () => {
-    return document.getElementById("senha-input");
+    return document.getElementById("senha-input").value
 }
 
 const cadastrarUsuario = () => {
-    console.log("call function cadastrar usuario")
+    emailMessage.innerHTML = ''
+    passwordMessage.innerHTML = ''
+
+    createUserWithEmailAndPassword(auth, getEmail(), getPassword())
+        .then((result) => {
+             console.log("usuario cadastrado com sucesso!" + JSON.stringify(result))
+
+            
+
+        })
+        .catch((error) => {
+            console.log("Erro ao cadastrar usuário: " + JSON.stringify(error))
+
+            switch(error.code) {
+                case "auth/invalid-email":
+                    emailMessage.innerHTML = "Email inválido"
+                    break;
+                case "auth/weak-password":
+                    passwordMessage.innerHTML = "Senha curta"
+                    break;    
+            }
+
+            if(error.code == "auth/invalid-email") {
+                emailMessage.innerHTML = "Email inválido"
+            }
+        })
 }
 
 document
